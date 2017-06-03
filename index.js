f86e0b9fa0c8419d7f10ee53d2a36b407e53115a
George <fran.man@hotmail.co.uk>
2017-06-03 23:28:47 +0100

    renamed app2 to app

--- index.js
+++ index.js
@@ -1,20 +1,19 @@
 const express = require('express')
-const app = express()
 
 process.argv.forEach(function(val, index, array){
-	app2 = express()
+	app = express()
 	if(index > 1) {
-		app2.get('/', function(req, res) {
+		app.get('/', function(req, res) {
 			console.log('Root hit on port: ' + val)
 			res.send('Hello!')
 		})
 
-		app2.get('/:param', function(req, res) {
+		app.get('/:param', function(req, res) {
 			console.log(req.params.param + ' endpoint hit!')
 			res.send('Hello from ' +req.params.param+ '!\nServer: ' + val)
 		})
 		
-		app2.listen(val, function() {
+		app.listen(val, function() {
 			console.log('Listening on port: ' + val)
 		})
 	}
