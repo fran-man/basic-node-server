f2e23d6221201010f6f88e2d798d4c52c498f88a
George <fran.man@hotmail.co.uk>
2017-06-03 23:26:18 +0100

    Support for multiple servers

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 const express = require('express')
 const app = express()
 
-app.get('/', function(req, res) {
-	console.log('Root hit!')
-	res.send('Hello!')
-})
+process.argv.forEach(function(val, index, array){
+	app2 = express()
+	if(index > 1) {
+		app2.get('/', function(req, res) {
+			console.log('Root hit on port: ' + val)
+			res.send('Hello!')
+		})
 
-app.get('/:param', function(req, res) {
-	console.log(req.params.param + ' endpoint hit!')
-	res.send('Hello from ' +req.params.param+ '!')
-})
-
-app.listen(1234, function() {
-	console.log('test')
+		app2.get('/:param', function(req, res) {
+			console.log(req.params.param + ' endpoint hit!')
+			res.send('Hello from ' +req.params.param+ '!\nServer: ' + val)
+		})
+		
+		app2.listen(val, function() {
+			console.log('Listening on port: ' + val)
+		})
+	}
 })
