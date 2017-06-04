687fd5470f68995af19ab742bcef824a8c5d4174
George <fran.man@hotmail.co.uk>
2017-06-04 15:53:07 +0100

    tweak to take one argument and listen on that port

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
 const express = require('express')
+const app = express()
+var instancePort
 
-process.argv.forEach(function(val, index, array){
-	app = express()
-	if(index > 1) {
-		app.get('/', function(req, res) {
-			console.log('Root hit on port: ' + val)
-			res.send('Hello!')
-		})
+if(process.argv[2] == undefined || process.argv[2] == null) {
+	instancePort = instancePort = 1234
+}
+else {
+	instancePort = process.argv[2]
+}
 
-		app.get('/:param', function(req, res) {
-			console.log(req.params.param + ' endpoint hit!')
-			res.send('Hello from ' +req.params.param+ '!\nServer: ' + val)
-		})
-		
-		app.listen(val, function() {
-			console.log('Listening on port: ' + val)
-		})
-	}
+app.get('/', function(req, res) {
+	console.log('Root hit on port: ' + instancePort)
+	res.send('Hello from instance running on port ' + instancePort + '!')
+})
+
+app.get('/:param', function(req, res) {
+	console.log(req.params.param + ' endpoint hit on server port ' +instancePort+ '!')
+	res.send('Hello from ' + req.params.param + '!\nServer: ' + instancePort)
+})
+
+app.listen(instancePort, function() {
+	console.log('Listening on port: ' + instancePort)
 })
