e9c11fa972d2b58d8c5f745c94be1fc7f64a4384
George <fran.man@hotmail.co.uk>
2017-06-04 21:38:30 +0100

    Added winston logging

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const logger = require('winston')
 var instancePort
 
 if(process.argv[2] == undefined || process.argv[2] == null) {
@@ -9,16 +10,22 @@ else {
 	instancePort = process.argv[2]
 }
 
+logger.configure({
+    transports: [
+      new (logger.transports.File)({ filename: 'logs/' + instancePort + '.log' })
+    ]
+  });
+
 app.get('/', function(req, res) {
-	console.log('Root hit on port: ' + instancePort)
+	logger.info('Root hit on port: ' + instancePort)
 	res.send('Hello from instance running on port ' + instancePort + '!')
 })
 
 app.get('/:param', function(req, res) {
-	console.log(req.params.param + ' endpoint hit on server port ' +instancePort+ '!')
+	logger.info(req.params.param + ' endpoint hit on server port ' +instancePort+ '!')
 	res.send('Hello from ' + req.params.param + '!\nServer: ' + instancePort)
 })
 
 app.listen(instancePort, function() {
-	console.log('Listening on port: ' + instancePort)
+	logger.info('Listening on port: ' + instancePort)
 })
